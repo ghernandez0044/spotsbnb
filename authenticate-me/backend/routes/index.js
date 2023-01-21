027c8bfe12c5d789ec22473af4c223f2d3a44a37
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-01-20 20:50:01 -0800

    completed phase 0, Initialize Express and Sequelize with CSRF protection

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/hello/world', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken())
-    res.send('Hello World!')
+router.get('/api/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken()
+    res.cookie('XSRF-TOKEN', csrfToken)
+    res.status(200).json({
+        "XRSF-Token": csrfToken
+    })
 })
 
 module.exports = router
