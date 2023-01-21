92ba3b5d8d36c5de3ec839256702f58bc73528b2
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-01-20 20:42:06 -0800

    tested server and it works

--- authenticate-me/backend/bin/www.js
+++ authenticate-me/backend/bin/www.js
@@ -1,7 +1,7 @@
 // Import environment variables
 require('dotenv').config()
 
-const port = { port } = require('../config')
+const { port } = require('../config')
 
 const app = require('../app')
 const db = require('../db/models')
@@ -9,15 +9,14 @@ const db = require('../db/models')
 // Check the database connection before starting the app
 db.sequelize
   .authenticate()
-  .then(() => [
+  .then(() => {
     console.log('Database connection success! Sequelize is ready to use!')
-  ])
 
-// Start listening for connections
-app.listen(port, () => [
-    console.log(`Listening on port ${port}...`)
-])
-  .catch((err) => {
-    console.log('Database connection failure.')
-    console.error(err)
+    // Start listening for connections
+    app.listen(port, () => console.log(`Listening on port ${port}...`));
   })
+  .catch((err) => {
+    console.log('Database connection failure.');
+    console.error(err);
+  });
+
