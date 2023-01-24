7b08ad559b962c8be7683ea0b0aa4eace3299cb3
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-01-23 22:56:11 -0800

    changed migration file

--- authenticate-me/backend/db/migrations/20230123184216-create-user.js
+++ authenticate-me/backend/db/migrations/20230123184216-create-user.js
@@ -8,8 +8,7 @@ if(process.env.NODE_ENV === 'production'){
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    options.tableName = 'Users'
-    await queryInterface.createTable(options, {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
