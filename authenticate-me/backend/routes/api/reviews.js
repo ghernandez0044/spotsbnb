cee41688158e285067f3fecc61bc75a70508e6a8
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-01-27 21:41:15 -0800

    recieved successful response from Add an Image to a Review based on the Reviews id

--- authenticate-me/backend/routes/api/reviews.js
+++ authenticate-me/backend/routes/api/reviews.js
@@ -46,6 +46,45 @@ router.get('/current', requireAuth, async (req, res, next) => {
 })
 
 
+// Add an Image to a Review based on the Review's id - require authentication = require authorization
+router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
+    const { url } = req.body
+    const review = await Review.findOne({
+        include: [
+            { model: ReviewImage }
+        ],
+        where: {
+            id: req.params.reviewId
+        }
+    })
+
+    if(!review){
+        const err = new Error("Review couldn't be found")
+        err.status = 400
+        next(err)
+    }
+
+    if(review.userId !== req.user.id){
+        const err = new Error("Review doesn't belong to user")
+        err.status = 400
+        next(err)
+    }
+
+    const numOfImg = review.toJSON().ReviewImages.length
+    
+    if(numOfImg >= 10){
+        const err = new Error('Maximum number of images for this resource was reached')
+        err.status = 403
+        next(err)
+    }
+
+    const reviewImage = await review.createReviewImage({ url })
+    
+    return res.status(200).json({
+        id: reviewImage.id,
+        url: reviewImage.url
+    })
+})
 
 
 
