0148d760e50d2b54064428d6193695a3d1588021
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-02-10 10:49:18 -0800

    setup React application and Redux integration

---
+++ authenticate-me/frontend/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import './index.css';
+import App from './App';
+import configureStore from './store';
+
+// Creating the store
+const store = configureStore();
+
+
+// If the environment is not production, set the store as a property on the window object to be able to interact with it on the browser console
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <ReduxProvider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReduxProvider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
