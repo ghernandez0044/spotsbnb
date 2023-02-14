2187b8d9ddf5df5a5715cbf25f46e565f3cb30ce
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-02-14 10:26:42 -0800

    added belongsToCurrentUser to spot details page

--- authenticate-me/frontend/src/components/SpotDetails/index.js
+++ authenticate-me/frontend/src/components/SpotDetails/index.js
@@ -11,8 +11,18 @@ function SpotDetails(){
     const spots = useSelector((state) => state.spots.Spots)
     const spot = normalizeData(spots)[id]
 
+    // Subscribe to user session slice of state
+    const currentUser = useSelector((state) => state.session.user)
+
+    console.log('user: ', currentUser)
+
     // Deconstruct needed properties from spot object
     const { address, avgRating, city, country, description, lat, lng, name, ownerId, previewImg, price, state } = spot
+    
+    // Check to see if current user owns this spot or not
+    const belongsToCurrentUser = currentUser.id === ownerId
+
+    console.log('current owner? ', belongsToCurrentUser)
 
     return (
         <div>
@@ -22,8 +32,11 @@ function SpotDetails(){
             <h3>Hosted by ... Owner {ownerId}</h3>
             <h4>Description</h4>
             <p>{description}</p>
-            <p>${price} /night</p>
-            <p>Average Rating: {avgRating} stars</p>
+            <div className='info-container'>
+                <p>${price} /night</p>
+                <p>Average Rating: {avgRating} stars</p>
+                {belongsToCurrentUser ? <p>You Own This Spot!</p> : <button>Reserve</button>}
+            </div>
         </div>
     )
 }
