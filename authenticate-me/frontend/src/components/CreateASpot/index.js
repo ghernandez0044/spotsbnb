37246138d625da015624c8b1697f98bbb7771207
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-03-02 10:23:00 -0800

    added error validations to create a spot page

--- authenticate-me/frontend/src/components/CreateASpot/index.js
+++ authenticate-me/frontend/src/components/CreateASpot/index.js
@@ -24,7 +24,7 @@ function CreateASpot({ edit, spot }){
     const [ lat, setLat ] = useState('')
     const [ lng, setLng ] = useState('')
     const [ price, setPrice ] = useState('')
-    const [ errors, setErrors ] = useState([])
+    const [ errors, setErrors ] = useState({})
     const [ isSubmitted, setIsSubmitted ] = useState(false)
 
     const [ previewImage, setPreviewImage ] = useState('')
@@ -34,129 +34,8 @@ function CreateASpot({ edit, spot }){
     const [ imageFive, setImageFive ] = useState('')
 
     console.log('spot: ', spot)
-
-    // Handle submission event
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setIsSubmitted(true)
-
-        setErrors([])
-
-        const errors = []
-
-        let countryError
-        if(country.length <= 0){
-            countryError = 'Country is required'
-            errors.push(countryError)
-        }
-
-        let addressError
-        if(address.length <= 0){
-            addressError = 'Address is required'
-            errors.push(addressError)
-        }
-
-        let cityError
-        if(city.length <= 0){
-            cityError = 'City is required'
-            errors.push(cityError)
-        }
-
-        let stateError
-        if(state.length <= 0){
-            stateError = 'State is required'
-            errors.push(stateError)
-        }
-
-        let latError
-        if(lat.length <= 0){
-            latError = 'Latitude is required'
-            errors.push(latError)
-        }
-
-        let lngError
-        if(lng.length <= 0){
-            lngError = 'Longitude is required'
-            errors.push(lngError)
-        }
-
-        let descriptionError
-        if(description.length < 30){
-            descriptionError = 'Description needs a minimum of 30 characters'
-            errors.push(descriptionError)
-        }
-
-        let nameError
-        if(name.length <= 0){
-            nameError = 'Name is required'
-            errors.push(nameError)
-        }
-
-        let priceError
-        if(price.length <= 0){
-            priceError = 'Price is required'
-            errors.push(priceError)
-        }
-
-        let previewImgError
-        if(previewImage.length <= 0){
-            previewImgError = 'Preview image is required'
-            errors.push(previewImgError)
-        }
-
-        const newSpotObj = {
-            name,
-            description,
-            city,
-            state,
-            country,
-            address,
-            lat,
-            lng,
-            price
-        }
-
-
-        if(edit && spot){
-            const editedSpot = await dispatch(editASpot(newSpotObj, spot.id)).catch(async (res) => {
-                const data = await res.json()
-                if(data && data.errors) errors.push(data.errors)
-            })            
-
-            history.replace(`/spots/${editedSpot.id}`)
-        } else {
-            const newSpot = await dispatch(createASpot(newSpotObj)).catch(async (res) => {
-                const data = await res.json()
-                if(data && data.errors) errors.push(data.errors)
-            })
-    
-            const previewImageObj = {
-                url: previewImage,
-                preview: true
-            }
-            const newPreviewImage = await dispatch(createAnImage(previewImageObj, newSpot.id)).catch(async (res) => {
-                const data = await res.json()
-                if(data && data.errors) errors.push(data.errors)
-            })
-    
-            let images = [imageTwo, imageThree, imageFour, imageFive]
-    
-            for(let image of images){
-                if(image !== ''){
-                    const imageObj = {
-                        url: image,
-                        preview: false
-                    }
-                    const newImage = await dispatch(createAnImage(imageObj, newSpot.id))
-                    if(newImage && newImage.errors) errors.push(newImage.errors)
-                }
-            }
-
-            history.replace(`/spots/${newSpot.id}`)
-        }
-
-
-        setErrors(errors)
+    // Function to reset all fields on form
+    const reset = () => {
         setName('')
         setDescription('')
         setCity('')
@@ -171,7 +50,122 @@ function CreateASpot({ edit, spot }){
         setImageThree('')
         setImageFour('')
         setImageFive('')
+    }
 
+    useEffect(() => {
+        const errors = {}
+
+        if(country.length <= 0){
+            errors.countryError = 'Country is required'
+        }
+
+        if(address.length <= 0){
+            errors.addressError = 'Address is required'
+        }
+
+        if(city.length <= 0){
+            errors.cityError = 'City is required'
+        }
+
+        if(state.length <= 0){
+            errors.stateError = 'State is required'
+            
+        }
+
+        if(lat.length <= 0){
+            errors.latError = 'Latitude is required'
+        }
+
+        if(lng.length <= 0){
+            errors.lngError = 'Longitude is required'
+        }
+
+        if(description.length < 30){
+            errors.descriptionError = 'Description needs a minimum of 30 characters'
+        }
+
+        if(name.length <= 0){
+            errors.nameError = 'Name is required'
+        }
+
+        if(price.length <= 0){
+            errors.priceError = 'Price is required'
+        }
+
+        if(previewImage.length <= 0){
+            errors.previewImgError = 'Preview image is required'
+        }
+
+        setErrors(errors)
+        console.log('useEffect errors: ', errors)
+        console.log('useEffect isSubmitted: ', isSubmitted)
+    }, [ name, description, city, state, country, address, lat, lng, price, previewImage ])
+
+    // Handle submission event
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setIsSubmitted(true)
+
+        if(Object.keys(errors).length === 0){
+            const newSpotObj = {
+                name,
+                description,
+                city,
+                state,
+                country,
+                address,
+                lat,
+                lng,
+                price
+            }
+    
+            if(edit && spot){
+                const editedSpot = await dispatch(editASpot(newSpotObj, spot.id)).catch(async (res) => {
+                    const data = await res.json()
+                    if(data && data.errors) errors.push(data.errors)
+                })            
+    
+                setIsSubmitted(false)
+                reset()
+                setErrors(errors)
+                history.replace(`/spots/${editedSpot.id}`)
+            } else {
+                const newSpot = await dispatch(createASpot(newSpotObj)).catch(async (res) => {
+                    const data = await res.json()
+                    if(data && data.errors) errors.push(data.errors)
+                })
+        
+                const previewImageObj = {
+                    url: previewImage,
+                    preview: true
+                }
+                const newPreviewImage = await dispatch(createAnImage(previewImageObj, newSpot.id)).catch(async (res) => {
+                    const data = await res.json()
+                    if(data && data.errors) errors.push(data.errors)
+                })
+        
+                let images = [imageTwo, imageThree, imageFour, imageFive]
+        
+                for(let image of images){
+                    if(image !== ''){
+                        const imageObj = {
+                            url: image,
+                            preview: false
+                        }
+                        const newImage = await dispatch(createAnImage(imageObj, newSpot.id))
+                        if(newImage && newImage.errors) errors.push(newImage.errors)
+                    }
+                }
+    
+                setIsSubmitted(false)
+                reset()
+                setErrors(errors)
+                history.replace(`/spots/${newSpot.id}`)
+            }
+        } else {
+            console.log('errors: ', errors)
+            console.log('isSubmitted: ', isSubmitted)
+        }
     }
 
     const store = useSelector(state => state)
@@ -203,61 +197,60 @@ function CreateASpot({ edit, spot }){
             {!edit ? <h1 style={{ textAlign: 'center' }}>Create A Spot</h1> : <h1 style={{ textAlign: 'center' }}>Edit A Spot</h1>}
             <form onSubmit={handleSubmit} className='create-a-spot-form'>
                 <ul>
-                    {errors.map((err, i) => {
-                        <li key={i}>{err}</li>
-                    })}
-                </ul>
-                <ul>
                     <div className='location-container'>
                         <h2>Where is your place located?</h2>
-                        <p><em>Guests will only get your exact address once they booked a reservation.</em></p>
+                        <p><em>Guests will only get your exact address once they have booked a reservation.</em></p>
                         <div className='location-inputs'>
                             <li>
-                                <label>Country:  <input type='text' defaultValue={!edit ? country : spot.country} onChange={(e) => setCountry(e.target.value)} placeholder='Country' required /></label>
+                                <label>Country: {isSubmitted && errors.countryError && ( <p className='errors'>{errors.countryError}</p> )} <input type='text' defaultValue={!edit ? country : spot.country} onChange={(e) => setCountry(e.target.value)} placeholder='Country' /></label>
                             </li>
                             <li>
-                                <label>Address: <input type='text' defaultValue={!edit ? address : spot.address} onChange={(e) => setAddress(e.target.value)} placeholder='Address' required /></label>
+                                <label>Address: {isSubmitted && errors.addressError && ( <p className='errors'>{errors.addressError}</p> )}<input type='text' defaultValue={!edit ? address : spot.address} onChange={(e) => setAddress(e.target.value)} placeholder='Address' /></label>
                             </li>
                             <li>
-                                <label>City: <input type='text' defaultValue={!edit ? city : spot.city} onChange={(e) => setCity(e.target.value)} placeholder='City' required /> </label>
+                                <label>City: {isSubmitted && errors.cityError && ( <p className='errors'>{errors.cityError}</p> )} <input type='text' defaultValue={!edit ? city : spot.city} onChange={(e) => setCity(e.target.value)} placeholder='City'  /> </label>
                             </li>
                             <li>
-                                <label>State: <input type='text' defaultValue={!edit? state : spot.state} onChange={(e) => setState(e.target.value)} placeholder='STATE' required /></label>
+                                <label>State: {isSubmitted && errors.stateError && ( <p className='errors'>{errors.stateError}</p> )} <input type='text' defaultValue={!edit? state : spot.state} onChange={(e) => setState(e.target.value)} placeholder='STATE' /></label>
                             </li>
                             <li>
-                                <label>Latitude: <input type='text' defaultValue={!edit ? lat : spot.lat} onChange={(e) => setLat(e.target.value)} placeholder='Latitude' required /></label>
+                                <label>Latitude: {isSubmitted && errors.latError && ( <p className='errors'>{errors.latError}</p> )} <input type='text' defaultValue={!edit ? lat : spot.lat} onChange={(e) => setLat(e.target.value)} placeholder='Latitude'  /></label>
                             </li>
                             <li>
-                                <label>Longitude: <input type='text' defaultValue={!edit ? lng : spot.lng} onChange={(e) => setLng(e.target.value)} placeholder='Longitude' required /></label>
+                                <label>Longitude: {isSubmitted && errors.lngError && ( <p className='errors'>{errors.lngError}</p> )} <input type='text' defaultValue={!edit ? lng : spot.lng} onChange={(e) => setLng(e.target.value)} placeholder='Longitude'  /></label>
                             </li>
                         </div>
                     </div>
                     <div className='description-container-create-spot'>
                         <h2>Describe your place to guests</h2>
+                        {isSubmitted && errors.descriptionError && ( <p className='errors'>{errors.descriptionError}</p> )}
                         <p><em>Mention the best features of your space, any special amentities like fast wifi or parking, and what you love about the neighborhood.</em></p>
                         <li>
-                            <textarea defaultValue={!edit ? description : spot.description} onChange={(e) => setDescription(e.target.value)} placeholder='Please write at least 30 characters' required />
+                            <textarea defaultValue={!edit ? description : spot.description} onChange={(e) => setDescription(e.target.value)} placeholder='Please write at least 30 characters'  />
                         </li>
                     </div>
                     <div className='name-container'>
                         <li>
                             <h2>Create a title for your spot</h2>
                             <p><em>Catch guests' attention with a spot title that highlights what makes your place special.</em></p>
-                            <input type='text' defaultValue={!edit ? name : spot.name} onChange={(e) => setName(e.target.value)} placeholder='Name of your spot' required />
+                            {isSubmitted && errors.nameError && ( <p className='errors'>{errors.nameError}</p> )}
+                            <input type='text' defaultValue={!edit ? name : spot.name} onChange={(e) => setName(e.target.value)} placeholder='Name of your spot'  />
                         </li>
                     </div>
                     <div className='price-container'>
-                        <h2>Set a base price for your spot</h2>
-                        <p><em>Competitive pricing can help your listing stand out and rank higher in search results</em></p>
                         <li>
-                            <label>$ <input type='text' defaultValue={!edit ? price : spot.price} onChange={(e) => setPrice(e.target.value)} placeholder='Price per night (USD)' required /></label>
+                            <h2>Set a base price for your spot</h2>
+                            <p><em>Competitive pricing can help your listing stand out and rank higher in search results</em></p>
+                            {isSubmitted && errors.priceError && ( <p className='errors'>{errors.priceError}</p> )}
+                            <label>$ <input type='text' defaultValue={!edit ? price : spot.price} onChange={(e) => setPrice(e.target.value)} placeholder='Price per night (USD)'  /></label>
                         </li>
                     </div>
                     <div className='photos-container'>
                         <h2>Liven up your spot with photos</h2>
                         <p><em>Submit a link to at least one photo to publish your spot,</em></p>
                         <div className='location-inputs'>
-                            <input type='text' defaultValue={!edit ? previewImage : previewImg} onChange={(e) => setPreviewImage(e.target.value)} placeholder='Preview Image URL' required />
+                            {isSubmitted && errors.previewImgError && ( <p className='errors'>{errors.previewImgError}</p> )}
+                            <input type='text' defaultValue={!edit ? previewImage : previewImg} onChange={(e) => setPreviewImage(e.target.value)} placeholder='Preview Image URL'  />
                             <input type='text' defaultValue={!edit ? imageTwo : regularImages[0] ? regularImages[0] : imageTwo} onChange={(e) => setImageTwo(e.target.value)} placeholder='Image URL'/>
                             <input type='text' defaultValue={!edit ? imageThree : regularImages[1] ? regularImages[1] : imageThree} onChange={(e) => setImageThree(e.target.value)} placeholder='Image URL'/>
                             <input type='text' defaultValue={!edit ? imageFour : regularImages[2] ? regularImages[2] : imageFour} onChange={(e) => setImageFour(e.target.value)} placeholder='Image URL'/>
