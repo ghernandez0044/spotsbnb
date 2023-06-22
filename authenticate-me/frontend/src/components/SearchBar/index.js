f753f67686116952398a9c847b4c463888160752
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-21 17:08:49 -0700

    completed some basic styling of search bar frontend component

--- authenticate-me/frontend/src/components/SearchBar/index.js
+++ authenticate-me/frontend/src/components/SearchBar/index.js
@@ -2,13 +2,17 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import './SearchBar.css'
 
 function SearchBar(){
 
 
     return (
         <div className='search-bar-container'>
-            <div>Search Bar Here</div>
+            <div className="search-icon-container">
+                <i className="fas fa-solid fa-magnifying-glass"></i>
+            </div>
+            <input type='search' placeholder="Search" spellCheck={true} required  />
         </div>
     )
 }
