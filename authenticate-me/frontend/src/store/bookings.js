ccee0bcb00a1e0417cad52288ee874fefc9b2ae2
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-06-23 16:52:09 -0700

    started bookings slice of state

---
+++ authenticate-me/frontend/src/store/bookings.js
@@ -0,0 +1,10 @@
+// Necessary imports
+import { csrfFetch } from "./csrf";
+import { normalizeData } from "./spots";
+
+// Create type strings
+const LOAD_BOOKINGS = 'bookings/loadBookings'
+const LOAD_BOOKING = 'bookings/loadBooking'
+const CREATE_BOOKING = 'bookings/createBooking'
+const DELETE_BOOKING = 'bookings/deleteBooking'
+
