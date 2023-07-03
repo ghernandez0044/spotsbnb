6bc288b3b29be7003894df25c22ce3f3e344081e
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-02 19:50:39 -0700

    progress check, customizing tailwind css config file

---
+++ authenticate-me/frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'phone': '390px',
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      'main-color': '#ff5a5f',
+      'accent-color': '#00a699',
+      'alternate-color': '#fc642d',
+      'dark-color': '#484848',
+      'light-color': '#767676',
+      'white': '#ffffff',
+      'transparent': 'transparent'
+    },
+    fontFamily: {
+        sans: ['Lobster Two', 'sans'],
+        serif: ['Signika Negative', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
