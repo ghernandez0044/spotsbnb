92dd96ef20633a842d70296068535ad5f2cc7a96
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-04 05:27:28 -0700

    refactored spot gallery card component to tailwind css

--- authenticate-me/frontend/src/components/SpotGalleryCard/index.js
+++ authenticate-me/frontend/src/components/SpotGalleryCard/index.js
@@ -51,29 +51,28 @@ function SpotGalleryCard({ spot, manage }){
     const load = () => {
         dispatch(getSpot(id)).then(res => loadSpot(id)).then(res => dispatch(getReviews(id))).then(res => dispatch(getUserBookings(user.id))).catch(async error => {
             const formattedError = await error.json()
-            console.log('error: ', formattedError)
         })
-    }  // when i click on the spot card, i want to dispatch a thunk to get all the user bookings
+    } 
 
 
     return (
         <li>
-            <div className="card" title={name}>
+            <div className="h-fit w-80 rounded-xl m-3.5 flex flex-col" title={name}>
                 <NavLink exact to={`/spots/${id}`} onClick={load}>
                     <img src={previewImage} alt='' />
-                    <div className="content-container">
-                        <div className='city-container'>
+                    <div className="flex flex-col justify-between py-0.5 px-2">
+                        <div className='h-10 flex justify-between items-center'>
                             <h4>{city}, {state}</h4>
                             {avgRating ? <p><i className='fa-solid fa-star' /> {avgRating}</p> : <p><i className='fa-solid fa-star' />New</p>}
                         </div>
-                        <div className='price-info-container'>
+                        <div className='h-9 flex justify-start items-center'>
                             <p><b>${Number(price).toFixed(2)}</b> /night</p>
                         </div>
                     </div>
                 </NavLink>
                 {
                     manage && (
-                        <div className='manage-buttons-container'>
+                        <div className='flex justify-evenly p-1.5 mb-1.5'>
                                 <button onClick={onClick} className='manage-button'>Update</button>
                             <OpenModalButton className='manage-button' modalComponent={<Confirmation label='Confirm Delete' message='Are you sure you want to remove this spot from the listings?' onYes={onYes} yesLabel='Delete Spot' onNo={onNo} noLabel='Keep Spot' />} buttonText='Delete' />
                         </div>
